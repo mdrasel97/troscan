6c47b7eb492bc14c676e143926d6def564fd71a2
mdrasel97 <mollarasel972@gmail.com>
2025-08-08 08:16:38 +0600

    banner section full responsive added

--- src/app/pages/Banner.jsx
+++ src/app/pages/Banner.jsx
@@ -7,24 +7,24 @@ export default function Banner() {
       style={{ backgroundImage: `url('/assets/banner.png')` }}
     >
       {/* Overlay for darker effect */}
-      <div className="absolute inset-0 bg-opacity-50 "></div>
+      <div className="absolute inset-0 bg-opacity-50"></div>
 
       {/* Content */}
       <div className="relative z-10 text-center text-white px-4">
-        <h1 className="text-3xl md:text-6xl w-4/6 mx-auto font-bold mb-6">
+        <h1 className="text-4xl md:text-6xl md:w-4/6 mx-auto font-bold mb-6">
           Timeless Comfort & Endless Luxury
         </h1>
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
           <Link href="/get-started">
-            <span className="hidden md:inline-block bg-white text-[#ad5744] px-6 font-bold py-3 rounded hover:bg-">
+            <button className="md:inline-block bg-white text-[#ad5744] px-6 font-bold py-3 rounded w-full">
               Our Vision
-            </span>
+            </button>
           </Link>
           <Link href="/learn-more">
-            <span className="hidden md:inline-block text-white hover:text-[#ad5744] px-6 font-bold py-3 rounded border border-white hover:bg-white">
+            <button className="md:inline-block w-full text-white hover:text-[#ad5744] px-6 font-bold py-3 rounded border border-white hover:bg-white">
               Explore Expertise
-            </span>
+            </button>
           </Link>
         </div>
       </div>
